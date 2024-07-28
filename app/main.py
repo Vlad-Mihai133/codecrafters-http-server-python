89abd56d9845e147f3b207c9117533623b21b9ab
Vlad-Mihai133 <vladmihai.ionescu133@gmail.com>
2024-07-28 16:34:03 +0300

    respond with user-agent

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     conn, addr = server_socket.accept()  # wait for client
 
     data = conn.recv(2048)
-    data_split = data.split(b"\r\n")
+    data_split = data.split(b"\r\n", maxsplit=1)
 
     response = "HTTP/1.1 200 OK\r\n\r\n"
     if data_split[0].split(b" ")[1] == b"/" or data_split[0].split(b" ")[1] == b"":
@@ -25,7 +25,7 @@ def main():
                         + "\r\n\r\n" + text)
             conn.sendall(response.encode())
         elif request_target == b"/user-agent":
-            user_agent = re.search(r"User-Agent: ([a-zA-Z0-9./]*)", data_split[1].decode()).group()
+            user_agent = re.search(r"User-Agent: ([\w./]*)", data_split[1].decode()).group()
             text = user_agent.split(" ")[1]
             content_length = str(len(text))
             response = ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + content_length
