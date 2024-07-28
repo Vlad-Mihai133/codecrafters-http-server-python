f8bde986f1325146625eaf1b663918fe3ba4546f
Vlad-Mihai133 <vladmihai.ionescu133@gmail.com>
2024-07-28 16:49:23 +0300

    added threads

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 import socket
 import re
+import threading
 
 HOST = "localhost"
 PORT = 4221
 
 
-def main():
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
-
+def handle(conn, addr):
     data = conn.recv(2048)
     data_split = data.split(b"\r\n", maxsplit=1)
 
@@ -19,12 +17,16 @@ def main():
         request_target = data_split[0].split(b" ")[1]
         request_target_split = request_target.split(b"/")
         if request_target_split[1] == b"echo" and len(request_target_split) == 3:
+            # Here we just extract the string from /echo/{str} request target
+            # after which we respond with that same {str} as a body message
             text = request_target_split[2].decode()
             content_length = str(len(text))
             response = ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + content_length
                         + "\r\n\r\n" + text)
             conn.sendall(response.encode())
         elif request_target == b"/user-agent":
+            # I used regex here to search for User-Agent literal string in case there would be more
+            # headers
             user_agent = re.search(r"User-Agent: ([\w./!@#$%^&*()+=-]*)", data_split[1].decode()).group()
             text = user_agent.split(" ")[1]
             content_length = str(len(text))
@@ -34,6 +36,15 @@ def main():
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
             conn.sendall(response.encode())
+
+
+def main():
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    conn, addr = server_socket.accept()  # wait for client
+    handle(conn, addr)
+    thread = threading.Thread(target=handle, args=(conn, addr))
+    thread.start()
+    thread.join()
 
 
 if __name__ == "__main__":
