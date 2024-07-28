3da0ae7dd5ef34eb9e2988aa42a92925521a3d4f
Vlad-Mihai133 <vladmihai.ionescu133@gmail.com>
2024-07-28 15:07:41 +0300

    2nd stage

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
     conn, addr = server_socket.accept()  # wait for client
     response_msg = "HTTP/1.1 200 OK\r\n\r\n"
-    conn.sendall(__data=response_msg.encode())
+    conn.sendall(response_msg.encode())
 
 
 if __name__ == "__main__":
